8081740d660e5574d265946fb378a3777e5ed14a
Ariharan D <ariharanduraisamy@gmail.com>
2023-12-11 13:06:02 +0530

    Add project files.

---
+++ WebAPIDapperDataAccessLayer/ITicketSP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPIDapperDataAccessLayer
+{
+    public interface ITicketSP
+    {
+        public void InsertSP(TicketModelSP Details);
+        public void DeleteSP(long PASSENGERID);
+        public List<TicketModelSP> ReadbyIDSP(long PASSENEGERID);
+        public List<TicketModelSP> ReadSP();
+        public void UpdateSP(int ticupd, TicketModelSP Details);
+    }
+}
---
+++ WebAPIDapperDataAccessLayer/TicketModelSP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIDapperDataAccessLayer
+{
+    public class TicketModelSP
+    {
+        public int PASSENGERID { get; set; }
+        public long TICKETNUMBER { get; set; }
+        public string PASSENGERNAME { get; set; }
+        public long PHNUMBER { get; set; }
+        public string EMAILID { get; set; }
+        public DateTime JOURNEYDATE { get; set; }
+    }
+}
